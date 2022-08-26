76fc5b07346f2e60b8ada1cd515bd47759b338ce
Cody <56003674+ctburenski@users.noreply.github.com>
2022-08-26 22:58:58 +0000

    Changing null/undefined check to a type check

--- backend/src/lib/authCheck.ts
+++ backend/src/lib/authCheck.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 async function authCheck(req: Request, res: Response, next: NextFunction) {
-	if (!req.session.username) {
+	if (typeof req.session.username !== 'string') {
 		res.status(401).end();
 	} else {
 		next();
